bb25f7598b2ad913dc610157650c6fa7e7d98c6c
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-10 16:29:20 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
 case "`lsb_release -is`" in
 
      Ubuntu)
              apt-get update; apt-get upgrade -y
+             apt-get install ruby git nano curl zip unzip -y
+             cd /opt
+             wget https://github.com/busyloop/lolcat/archive/master.zip
+             unzip master.zip
+             rm -Rf master.zip
+             cd lolcat-master/
+             gem install lolcat
+             wget 
      ;;
      CentOS)
              yum -y update
