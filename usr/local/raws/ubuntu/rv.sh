e172905eb1c9659e2f8a5d608e34ee4414b92100
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-11 12:23:54 -0700

    Create rv.sh

---
+++ usr/local/raws/ubuntu/rv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ "$1" == '-v' ]]; then
+clear
+echo ""
+echo -n "
+________ _______ ___       __
+___  __ \___    |__ |     / /
+__  /_/ /__  /| |__ | /| / /
+_  _, _/ _  ___ |__ |/ |/ /
+/_/ |_|  /_/  |_|____/|__/
+                             " | lolcat -a -s 100
+echo ""
+echo ""
+echo "************************" | lolcat -a -s 100
+echo ""
+echo "Stable 0.5 ." | lolcat -a -s 100
+echo "Simple Ubuntu Cli Helper ." | lolcat -a -s 100
+echo ""
+echo "************************" | lolcat -a -s 100
+echo ""
+fi
