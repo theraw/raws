c7c49e71fd09546e76567fbb69d217569efc111c
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-15 00:46:45 -0700

    Update si.sh

--- usr/local/raws/ubuntu/si.sh
+++ usr/local/raws/ubuntu/si.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 if [[ "$1" == 'sysinfo' ]];
 then
 
@@ -8,13 +8,17 @@ lscpu | grep Architecture: > ~/x.txt
 sed -ri 's/Architecture://g' ~/x.txt
 sed -ri 's/          //g' ~/x.txt
 chmod +x ~/x.txt
-x=$(cat ~/x.sh)
-
+x=$(cat ~/x.txt)
+cpu=$(grep -c processor /proc/cpuinfo)
 
 echo -n "|-------------------------------------------|" | lolcat -a -s 100
 echo -n "|**********| You Are Using.      |**********|" | lolcat -a -s 100
 echo -n "|**********| $distro. |**********|" | lolcat -a -s 100
-echo -n "|**********| $x. |**********|" | lolcat -a -s 100
+echo -n "|**********| $x.             |**********|" | lolcat -a -s 100
+echo -n "|**********| With : $cpu CPU/s.             |**********|" | lolcat -a -s 100
 echo -n "|-------------------------------------------|" | lolcat -a -s 100
 
+
+#Clear.
+rm -Rf ~/x.txt
 fi
