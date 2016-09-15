b642311cfe677775cb86df63a8f6e54c86dbb19e
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-15 11:10:05 -0700

    Update install-iptables.sh

--- usr/local/raws/ubuntu/install-iptables.sh
+++ usr/local/raws/ubuntu/install-iptables.sh
@@ -11,4 +11,12 @@ sudo iptables -A INPUT -j DROP
 sudo apt-get install iptables-persistent
 sudo invoke-rc.d iptables-persistent save
 iptables-save > ~/.firewall.conf
+sleep 0.5
+clear
+echo "Iptables firewall is now installed."
+echo "We just have allow access for port 22."
+echo "So you now will need to allow access for other ports which you want."
+echo "You can open/banip/unban/ etc. wrote 'raws ipt -h' to read how."
+echo "You firewall file is under `whoami` home. to read it just execute cat ~/.firewall.conf"
+
 fi
