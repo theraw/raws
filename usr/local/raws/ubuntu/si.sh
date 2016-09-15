c2eefd5676689f33cc1a6a7499b4d46485bcf917
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-15 01:45:26 -0700

    Update si.sh

--- usr/local/raws/ubuntu/si.sh
+++ usr/local/raws/ubuntu/si.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-clear
 if [[ "$1" == 'sysinfo' ]];
 then
-
+clear
 distro=$(cat /etc/issue.net)
 lscpu | grep Architecture: > ~/x.txt
 sed -ri 's/Architecture://g' ~/x.txt
@@ -16,6 +15,11 @@ x=$(cat ~/x.txt)
 cpu=$(grep -c processor /proc/cpuinfo)
 ram=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=4; {}/1024^2" | bc)
 ck=$(cat ~/mhz.txt)
+ip=$(curl -s https://api.ipify.org)
+cd /usr/local/raws/ubuntu/
+echo -n "We Are Testing Your Internet Speed Please Wait..." | lolcat -a -s 100; ./st.py > ~/speed.txt; echo "Done.";
+up=$(egrep "Upload:" ~/speed.txt)
+down=$(egrep "Download" ~/speed.txt)
 
 echo -n "|-------------------------------------------|" | lolcat -a -s 100
 echo -n "|**********|       ~SYSTEM~      |**********|" | lolcat -a -s 100
@@ -23,13 +27,19 @@ echo -n "|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*|" | lolcat -a -s 100
 echo -n "|**********| You Are Using.      |**********|" | lolcat -a -s 100
 echo -n "|**********| $distro. |**********|" | lolcat -a -s 100
 echo -n "|**********| $x.             |**********|" | lolcat -a -s 100
-echo -n "|**********| With : $cpu CPU/s.     |**********|" | lolcat -a -s 100
+echo -n "|**********| With  : $cpu CPU/s.    |**********|" | lolcat -a -s 100
 echo -n "|**********| Clock : $ck    |**********|" | lolcat -a -s 100
-echo -n "|**********| RAM : $ram GB.     |**********|" | lolcat -a -s 100
+echo -n "|**********| RAM   : $ram GB.  |**********|" | lolcat -a -s 100
 echo -n "|-------------------------------------------|" | lolcat -a -s 100
-
+echo -n "|-------------------------------------------|" | lolcat -a -s 100
+echo -n "|**********|      ~NETWORK~      |**********|" | lolcat -a -s 100
+echo -n "|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*|" | lolcat -a -s 100
+echo -n "|**********| IP : $ip |*********|" | lolcat -a -s 100
+echo -n "|**********| $up |********|" | lolcat -a -s 100
+echo -n "|**********| $down |*******|" | lolcat -a -s 100
 
 #Clear.
 rm -Rf ~/x.txt
 rm -Rf ~/mhz.txt
+rm -Rf ~/speed.txt
 fi
