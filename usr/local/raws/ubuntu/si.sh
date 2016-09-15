2455620e7633a852c43b7aa1e87106bef2d83e7b
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-15 00:41:37 -0700

    Update si.sh

--- usr/local/raws/ubuntu/si.sh
+++ usr/local/raws/ubuntu/si.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [[ "$1" == 'sysinfo' ]]; 
+if [[ "$1" == 'sysinfo' ]];
 then
 
 distro=$(cat /etc/issue.net)
+lscpu | grep Architecture: > ~/x.txt
+sed -ri 's/Architecture://g' ~/x.txt
+sed -ri 's/          //g' ~/x.txt
+chmod +x ~/x.txt
+x=$(cat ~/x.sh)
 
 
-echo -n "|**********| You Are Using. |**********|" | lolcat -a -s 100
+echo -n "|-------------------------------------------|" | lolcat -a -s 100
+echo -n "|**********| You Are Using.      |**********|" | lolcat -a -s 100
 echo -n "|**********| $distro. |**********|" | lolcat -a -s 100
+echo -n "|**********| $x. |**********|" | lolcat -a -s 100
+echo -n "|-------------------------------------------|" | lolcat -a -s 100
 
 fi
