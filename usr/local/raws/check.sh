5eee0668fcf722fe90499ef2fd360474eeacf365
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-11-12 20:47:33 +0000

    Update check.sh

--- usr/local/raws/check.sh
+++ usr/local/raws/check.sh
@@ -2,8 +2,9 @@
 curl -s 'https://raw.githubusercontent.com/systemroot/raws-dev/master/releases' > /tmp/rv.txt
 if [[ "`cat /tmp/rv.txt`" == '0.5' ]]; then
        rm -Rf /tmp/rv.txt
+       clear
    else
-      sleep 5
-      echo "If you can read this then you like to use raws cli, so to get latest update"
-      echo "please execute 'raws update' because a new version is out!!"
+      sleep 3
+      echo "If you can read this then you like to use raws cli, so to get latest update" | lolcat -a -s 100
+      echo "please execute 'raws update' because a new version is out!!" | lolcat -a -s 100
 fi
