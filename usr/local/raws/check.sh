c65be5a4670501ca2f9debf330ebb0f38e3456fc
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-11-13 22:35:54 +0000

    Update check.sh

--- usr/local/raws/check.sh
+++ usr/local/raws/check.sh
@@ -2,7 +2,6 @@
 curl -s 'https://raw.githubusercontent.com/systemroot/raws-dev/master/releases' > /tmp/rv.txt
 if [[ "`cat /tmp/rv.txt`" == '0.5' ]]; then
        rm -Rf /tmp/rv.txt
-       clear
    else
       sleep 3
       echo "If you can read this then you like to use raws cli, so to get latest update" | lolcat -a -s 100
