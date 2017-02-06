420a745b3a4955e6df42afda7f9be80f1c4ccd0b
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-02-06 10:06:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ case "`lsb_release -is`" in
              wget https://raw.githubusercontent.com/systemroot/raws-dev/master/usr/local/raws/ubuntu/bye.sh
              wget https://raw.githubusercontent.com/systemroot/raws-dev/master/usr/local/raws/ubuntu/update.sh
              wget https://raw.githubusercontent.com/systemroot/raws-dev/master/usr/local/raws/ubuntu/env.sh
+             wget https://raw.githubusercontent.com/systemroot/raws-dev/master/usr/local/raws/ubuntu/install-iptables.sh
              chmod +x *
              cd /usr/local/raws/
              wget https://raw.githubusercontent.com/systemroot/raws-dev/master/usr/local/raws/check.sh
