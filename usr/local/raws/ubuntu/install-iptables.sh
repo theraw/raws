896e35bcb438daab37223170e560e00d0beadb4e
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-02-06 10:25:41 +0000

    Update install-iptables.sh

--- usr/local/raws/ubuntu/install-iptables.sh
+++ usr/local/raws/ubuntu/install-iptables.sh
@@ -7,6 +7,7 @@ apt-get install iftop -y | lolcat -a -s 100
 clear
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT 1 -i lo -j ACCEPT
 sudo iptables -P INPUT DROP
 sudo iptables -A INPUT -j DROP
