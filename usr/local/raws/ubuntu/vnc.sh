54e23685bc691b90effc7bf60ecb8e3b22600662
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-02-24 08:35:03 +0200

    Update vnc.sh

--- usr/local/raws/ubuntu/vnc.sh
+++ usr/local/raws/ubuntu/vnc.sh
@@ -7,12 +7,23 @@ vnc-pro() {
 #End User.
 mkdir /vnc-pro/
 cd /vnc-pro/
+#Download.
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-cloud-server-evaluation_5.2.11_2_amd64.deb
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-terminal-server-evaluation_5.2.11_2_amd64.deb
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-workstation-evaluation_5.2.11_2_amd64.deb
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-plugin_5.2.11_1_i386.deb
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-enterprise-client_5.2.11_1_amd64.deb
 wget http://download.nomachine.com/download/5.2/Linux/nomachine-terminal-server-node-evaluation_5.2.11_2_amd64.deb
+#End Download.
+
+#Install
+dpkg -i nomachine-cloud-server-evaluation_5.2.11_2_amd64.deb
+dpkg -i nomachine-enterprise-client_5.2.11_1_amd64.deb
+dpkg -i nomachine-plugin_5.2.11_1_i386.deb
+dpkg -i nomachine-terminal-server-evaluation_5.2.11_2_amd64.deb
+dpkg -i nomachine-terminal-server-node-evaluation_5.2.11_2_amd64.deb
+dpkg -i nomachine-workstation-evaluation_5.2.11_2_amd64.deb
+#End Install.
 
 #Plugins 
 mkdir /vnc-pro/plugins/
